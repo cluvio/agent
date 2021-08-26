f41f35c84fb4114c487fd9e4ca3333b77a70268a
Ian Formanek <ian@cluvio.com>
2021-08-26 10:52:42 +0200

    Quote certificate password

--- scripts/apple-codesign.sh
+++ scripts/apple-codesign.sh
@@ -11,7 +11,7 @@ echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
 security create-keychain -p $KEYCHAIN_PASS build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p $KEYCHAIN_PASS build.keychain
-security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+security import certificate.p12 -k build.keychain -P "$MACOS_CERTIFICATE_PWD" -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEYCHAIN_PASS build.keychain
 security find-identity
 IDENTITY=$(security find-identity)
