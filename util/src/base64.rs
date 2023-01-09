15798ab730e1564d0c4b3f5f68b8502dd75ac5fb
Roman <roman@cluvio.com>
2023-01-09 13:35:19 +0100

    Dependency updates

--- util/src/base64.rs
+++ util/src/base64.rs
@@ -1,10 +1,12 @@
+use base64::Engine;
+
 /// Convert to URL-safe base64 string without padding.
 pub fn encode<T: AsRef<[u8]>>(bytes: T) -> String {
-    base64::encode_config(bytes.as_ref(), base64::URL_SAFE_NO_PAD)
+    base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(bytes.as_ref())
 }
 
 /// Convert from URL-safe base64 string without padding.
 pub fn decode(s: &str) -> Option<Vec<u8>> {
-    base64::decode_config(s, base64::URL_SAFE_NO_PAD).ok()
+    base64::engine::general_purpose::URL_SAFE_NO_PAD.decode(s).ok()
 }
 
