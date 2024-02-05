2c10847927a8c2d14b885a85a2cebed02c628594
Roman <roman@cluvio.com>
2024-02-05 12:39:56 +0100

    Try to upgrade the MacOS build to use notarytool.

--- scripts/macos/apple-notarize.sh
+++ scripts/macos/apple-notarize.sh
@@ -21,39 +21,11 @@ if [ -z "$APPLE_DEV_PASSWORD" ]; then
     exit 1
 fi
 
-xcrun altool --notarize-app \
-    --username "$APPLE_DEV_ACCOUNT" \
+xcrun notarytool submit \
+    --apple-id "$APPLE_DEV_ACCOUNT" \
     --password "$APPLE_DEV_PASSWORD" \
-    --file "${dmg_name}.dmg" \
-    --primary-bundle-id "com.cluvio.$name" | tee output
+    --wait \
+    "${dmg_name}.dmg"
 
-uuid=$(grep RequestUUID output | awk '{print $3}')
-
-if [ -z "$uuid" ]; then
-    echo "Missing RequestUUID in notarize output."
-    exit 1
-fi
-
-for i in $(seq 1 60); do
-    xcrun altool --notarization-info \
-        "$uuid" \
-        --username "$APPLE_DEV_ACCOUNT" \
-        --password "$APPLE_DEV_PASSWORD" | tee output
-    case $(grep "Status:" output | awk '{print $2}') in
-        "invalid")
-            echo "status = invalid"
-            exit 1
-            ;;
-        "success")
-            xcrun stapler staple "${dmg_name}.dmg"
-            exit 0
-            ;;
-        *)
-            sleep 30
-            ;;
-    esac
-done
-
-echo "Timeout when notarizing the app."
-exit 1
+xcrun stapler staple "${dmg_name}.dmg"
 
